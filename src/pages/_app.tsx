c40b4e7bdf5bd787f3b6a4d4d2942bc6162c0a2d
effe-m <francesco.megna97@gmail.com>
2021-06-20 16:56:59 +0200

    Add initial PWA setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,26 @@ import { AppProps } from 'next/app'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>NextJS TailwindCSS TypeScript Starter</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>KeepSketch - No-fluff money tracker</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name='application-name' content='PWA App' />
+				<meta name='apple-mobile-web-app-capable' content='yes' />
+				<meta name='apple-mobile-web-app-status-bar-style' content='default' />
+				<meta name='apple-mobile-web-app-title' content='PWA App' />
+				<meta name='description' content='Best PWA App in the world' />
+				<meta name='format-detection' content='telephone=no' />
+				<meta name='mobile-web-app-capable' content='yes' />
+				<meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+				<meta name='msapplication-TileColor' content='#2B5797' />
+				<meta name='msapplication-tap-highlight' content='no' />
+				<meta name='theme-color' content='#000000' />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
